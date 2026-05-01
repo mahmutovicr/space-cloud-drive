a971765ea8c1ec3d07b89148e9ff064613394ef6
Rahman Mahmutović <mahmutovicrahman5@gmail.com>
2026-05-02 00:46:41 +0200

    Refactor PostHogPageView for type safety and clarity
    Refactor PostHogPageView function to include type annotation and simplify URL construction.

--- src/app/_providers/pageview-tracker.tsx
+++ src/app/_providers/pageview-tracker.tsx
@@ -4,16 +4,17 @@ import { usePathname, useSearchParams } from "next/navigation";
 import { useEffect } from "react";
 import { usePostHog } from "posthog-js/react";
 
-export default function PostHogPageView() {
+export default function PostHogPageView(): null {
+  const posthog = usePostHog();
   const pathname = usePathname();
   const searchParams = useSearchParams();
-  const posthog = usePostHog();
 
   useEffect(() => {
     if (pathname && posthog) {
       let url = window.origin + pathname;
-      const search = searchParams.toString();
-      if (search) url += "?" + search;
+      if (searchParams.toString()) {
+        url = url + `?${searchParams.toString()}`;
+      }
       posthog.capture("$pageview", { $current_url: url });
     }
   }, [pathname, searchParams, posthog]);
