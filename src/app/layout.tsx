8d5674ea518c44ce6ccf90599f491aa57b6ad298
Rahman Mahmutović <mahmutovicrahman5@gmail.com>
2026-05-03 17:42:30 +0200

    Add viewport configuration to RootLayout
    Added viewport configuration to layout.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,18 @@
 import "~/styles/globals.css";
 
 import { GeistSans } from "geist/font/sans";
-import { type Metadata } from "next";
+import { type Metadata, type Viewport } from "next";
 import { PostHogProvider } from "./_providers/posthog-provider";
 
 export const metadata: Metadata = {
   title: "Space",
   description: "Secure, fast and easy file storage for the modern web",
   icons: [{ rel: "icon", url: "/favicon.svg", type: "image/svg+xml" }],
-  viewport: "width=device-width, initial-scale=1",
+};
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
 };
 
 export default function RootLayout({
