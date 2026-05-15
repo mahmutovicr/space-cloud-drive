39184fb0ee88fcfdf307b2a971aa306b33cfb226
Rahman Mahmutović <mahmutovicrahman5@gmail.com>
2026-05-15 18:29:01 +0200

    Add dynamic export to page.tsx

--- src/app/(home)/drive/page.tsx
+++ src/app/(home)/drive/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import { redirect } from "next/navigation";
 import { MUTATIONS, QUERIES } from "~/server/db/queries";
 import { DEMO_USER_ID } from "~/lib/mock-data";
