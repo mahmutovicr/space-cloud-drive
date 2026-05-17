9fe893ca653c06b2f20295ece98d5177708f065f
Rahman Mahmutović <mahmutovicrahman5@gmail.com>
2026-05-17 17:37:29 +0200

    Refactor database connection handling
    Refactor database connection to use createConnection instead of createPool and update getDb function.

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,33 +1,16 @@
 import { drizzle } from "drizzle-orm/singlestore";
-import { createPool, type Pool } from "mysql2/promise";
+import { createConnection } from "mysql2/promise";
 import { env } from "~/env";
 import * as schema from "./schema";
 
-const globalForDb = globalThis as unknown as {
-  conn: Pool | undefined;
-};
-
-const conn =
-  globalForDb.conn ??
-  createPool({
+export async function getDb() {
+  const conn = await createConnection({
     host: env.SINGLESTORE_HOST,
     port: parseInt(env.SINGLESTORE_PORT),
     user: env.SINGLESTORE_USER,
     password: env.SINGLESTORE_PASS,
     database: env.SINGLESTORE_DB_NAME,
     ssl: {},
-    waitForConnections: true,
-    connectionLimit: 10,
-    maxIdle: 10,
-    idleTimeout: 60000,
-    enableKeepAlive: true,
-    keepAliveInitialDelay: 10000,
   });
-
-globalForDb.conn = conn;
-
-conn.addListener("error", (err) => {
-  console.error("Database connection error:", err);
-});
-
-export const db = drizzle(conn, { schema });
+  return drizzle(conn, { schema });
+}
