d6752fd66b87a3a6b4fdbcab05262843e9d38bc0
Rahman Mahmutović <mahmutovicrahman5@gmail.com>
2026-05-17 17:39:35 +0200

    Replace PostHogProvider with ClerkProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import "~/styles/globals.css";
 
 import { GeistSans } from "geist/font/sans";
 import { type Metadata, type Viewport } from "next";
-import { PostHogProvider } from "./_providers/posthog-provider";
+import { ClerkProvider } from "@clerk/nextjs";
 
 export const metadata: Metadata = {
   title: "Space",
@@ -19,10 +19,10 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={`${GeistSans.variable}`}>
-      <body>
-        <PostHogProvider>{children}</PostHogProvider>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en" className={`${GeistSans.variable}`}>
+        <body>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
